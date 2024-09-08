e096938093137af97be0ee97e7d9d4395016a8f8
amit231 <amitsharma0843@gmail.com>
2024-09-08 21:34:10 +0530

    WIP

--- main.cpp
+++ main.cpp
@@ -1,75 +1,79 @@
-#include <bits/stdc++.h>
+// No other way & never give up.
+#include<bits/stdc++.h>
 using namespace std;
-#define ll long long
+#define deb(x)cout<<#x<<": "<<x<<'\n';
+int m =1e9+7;
+#define vi vector<int>
+#define vvi vector<vi >
+#define vb vector<bool>
+#define vvb vector<vb >
+ 
+int res;
+vector<pair<int,int>> dp;
+pair<int,int> dfs(int root, vvi& g,int p=-1){      
+    vi subroots_with_edges,subroots_without_edges;
+    int sum=0;
+    for(auto& ch:g[root]){
+        if(p==ch)   continue;
+        auto v=dfs(ch,g,root);
+        sum+=v.second;
+        subroots_with_edges.push_back(v.second);
+        subroots_without_edges.push_back(v.first);
+    }   
+    int sz=subroots_with_edges.size();
+    int ans=0;
+    // if include - sum(2's subtrees) + 2 + 4
 
-void solve()
+    for(int i=0;i<subroots_with_edges.size();i++){
+        // exclude it from sum with edge.
+        int include = sum-subroots_with_edges[i]; 
+
+        // include its childrens i.e wo edges.
+        include += subroots_without_edges[i];
+        ans=max(ans,1+include);
+    }
+    return {sum,ans};
+}
+void solve(){
+    int n=1,m=0;
+    string s;
+    cin>>n;
+    res=0;
+    dp.assign(n,{0,0});
+    int e=n-1;
+    vector<vector<int>> g(n+1);
+    for(int i=1;i<=e;i++){
+      int u,v; cin>>u>>v;
+      u--,v--;
+      g[u].push_back(v);
+      g[v].push_back(u);
+    }
+    // I can root it any point. let's root it at 0?
+    auto ans = dfs(0,g);
+    cout<<ans.first<<" "<<ans.second;
+}
+ 
+int32_t main()
 {
-  ll n;
-  cin >> n;
-
-  vector<ll> a(n);
-
-  map<ll, ll> mp;
-  for (int i = 0; i < n; i++)
-    cin >> a[i];
-
-  ll ans = 0;
-  vector<ll> left(n, 0);
-  ll mx = a[0], k = 1;
-  ll r = n - 2;
-
-  for (int i = 1; i < n; i++)
-  {
-    left[i] = max(mx + k, a[i]) + r--;
-    if (mx + k < a[i])
+ 
+ ios_base::sync_with_stdio(false);
+ cin.tie(NULL);
+ 
+    int T = 1;
+    // cin >> T;
+    while (T--)
     {
-      mx = a[i];
-      k = 1;
+        solve();
     }
-    else
-      k++;
-  }
-  // for(int i=0;i<n;i++)
-  // cout<<left[i]<<" ";
-  // cout<<"\n";
-  vector<ll> right(n, 0);
-  r = n - 2;
-  mx = a[n - 1], k = 1;
-  for (int i = n - 2; i >= 0; i--)
-  {
-    right[i] = max(mx + k, a[i]) + r--;
-    if (mx + k < a[i])
-    {
-      mx = a[i];
-      k = 1;
-    }
-    else
-      k++;
-  }
-  // for(int i=0;i<n;i++)
-  // cout<<right[i]<<" ";
-  // cout<<"\n";
-  ans = 1e19;
-  for (int i = 0; i < n; i++)
-  {
-    // ans = max(ans, a[i]);
-
-    //  ans= max(ans, left[i]);
-
-    ans = min(ans, max({left[i], a[i], right[i]}));
-  }
-
-  // if(mp[ans] == 1) ans--;
-  cout << ans << "\n";
+    return 0;
 }
 
-int main()
-{
-  ll t = 1;
-  // cin>>t;
+/*!SECTION
 
-  while (t--)
-    solve();
 
-  return 0;
-}
+
+5
+1 2
+1 3
+3 4
+3 5*/
